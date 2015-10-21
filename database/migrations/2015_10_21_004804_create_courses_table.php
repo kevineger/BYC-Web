8c5ff0f912a870d84c60ccdc7ba3f9ff352e4ef3
Kevin <kevin.eger@hotmail.com>
2015-10-20 18:30:10 -0700

    Adds core models and migrations

---
+++ database/migrations/2015_10_21_004804_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->string('name');
+            $table->mediumText('description');
+            $table->string('address');
+            $table->boolean('active')->default(false);
+            $table->integer('min_age');
+            $table->integer('max_age');
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('courses');
+    }
+}
