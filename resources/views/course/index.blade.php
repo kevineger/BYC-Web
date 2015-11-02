5f7f0e39ccf983111174896609ea9984ce70fda4
Shannon Farvolden <shannonfarvolden@hotmail.com>
2015-11-02 01:30:48 -0800

    displays schools and courses in boxes, columns of three

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -4,7 +4,23 @@
     <h1>Course Index</h1>
     <ul>
         @foreach( $courses as $course )
-            <li><a href="{{ action('CoursesController@show', [$course]) }}">{{ $course->name }}</a></li>
+            <div class="col-md-4 col-sm-4">
+                <a href="{{ action('CoursesController@show', [$course]) }}">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">{{$course->name}}</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div>
+                                <h7>{{$course->school->name}}</h7>
+                            </div>
+                            <div>
+                                <h9>Price: ${{$course->price}}</h9>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
         @endforeach
     </ul>
 @endsection
--- resources/views/school/index.blade.php
+++ resources/views/school/index.blade.php
@@ -4,7 +4,24 @@
     <h1>School Index</h1>
     <ul>
         @foreach( $schools as $school )
-            <li><a href="{{ action('SchoolsController@show', [$school]) }}">{{ $school->name }}</a></li>
+            <div class="col-md-4 col-sm-4">
+            <a href="{{ action('SchoolsController@show', [$school]) }}">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{$school->name}}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div>
+                            <h7>Vendor: {{$school->user->name}}</h7>
+                        </div>
+                        <div>
+                            <h9>Address: {{$school->address}}</h9>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            </div>
+            {{--<li><a href="{{ action('SchoolsController@show', [$school]) }}">{{ $school->name }}</a></li>--}}
         @endforeach
     </ul>
 @endsection
