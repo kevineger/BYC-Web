bba37af34ac2bc5f740cb8aa4c2902d516062609
kevineger <egerkevinjames@gmail.com>
2016-01-31 20:10:19 -0800

    Photos for profiles

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -54,6 +54,9 @@ Route::resource('users', 'UsersController', [
         'store'
     ]
 ]);
+Route::post('users/{users}/addPhoto', ['as' => 'addPhotoToUser', 'uses' => 'UsersController@addPhoto']);
+Route::post('users/{users}/removePhoto', ['as' => 'removePhotoFromUser', 'uses' => 'UsersController@removePhoto']);
+
 Route::get('dashboard', 'DashboardController@show');
 
 Route::get('search', 'SearchController@index');
