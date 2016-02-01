92ce5cd50d503a8129c6e0ea354ba161f33628d3
Shannon Farvolden <shannonfarvolden@hotmail.com>
2016-02-01 00:13:37 -0800

    merge sematic ui

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,39 +1,21 @@
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="{{ url('/') }}">BYC</a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/schools') }}">Schools</a></li>
-                <li><a href="{{ url('/courses') }}">Courses</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ action('CartController@index') }}">Cart</a></li>
-                @if (Auth::guest())
-                    <li><a href="{{ url('/auth/login') }}">Login</a></li>
-                    <li><a href="{{ url('/auth/register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true"
-                           aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-
-                            <li><a href="{{ action('UsersController@show', [Auth::user()]) }}">Profile</a></li>
-                            <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-
-            </ul>
+<div class="ui large menu">
+    <div class="ui container">
+        <a href="{{ url('/schools') }}" class="item">Schools</a>
+        <a href="{{ url('/courses') }}" class="item">Courses</a>
+        <div class="right menu">
+            <a href="{{ action('CartController@index') }}" class="item">Cart</a>
+            @if (Auth::guest())
+                <a href="{{ url('/auth/login') }}" class="item">Login</a>
+                <a href="{{ url('/auth/register') }}" class="item">Register</a>
+            @else
+                <div class="ui simple dropdown item">
+                    {{ Auth::user()->name }} <i class="dropdown icon"></i>
+                    <div class="menu">
+                        <a href="{{ action('UsersController@show', [Auth::user()]) }}" class="item">Profile</a>
+                        <a href="{{ url('/auth/logout') }}" class="item">Logout</a>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
-</nav>
+</div>
