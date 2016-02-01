275031292fba0e7ea26a83c0e346c79871042946
Shannon Farvolden <shannonfarvolden@hotmail.com>
2016-01-31 20:10:55 -0800

    adds some course UI

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -8,26 +8,23 @@
         {!! Form::input('search', 'q', null, ['class' => 'form-control', 'placeholder' => 'Search...']) !!}
     </div>
     {!! Form::close() !!}
+    <div class="row">
     @foreach( $courses as $course )
         @if($course->active)
-            <div class="col-md-4 col-sm-4">
-                <a href="{{ action('CoursesController@show', [$course]) }}">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">{{$course->name}}</h3>
-                        </div>
-                        <div class="panel-body">
-                            <div>
-                                <h7>{{$course->school->name}}</h7>
-                            </div>
-                            <div>
-                                <h9>Price: ${{$course->price}}</h9>
-                            </div>
+            <div class="col-sm-6 col-md-4">
+                <a href="{{ action('CoursesController@show', [$course]) }}" class="thumbnail-link">
+                    <div class="thumbnail">
+                        <img src="/photos/courses/1453338241-2013-12-19 15.59.28.png">
+                        <div class="caption">
+                            <h3>{{$course->name}}</h3>
+                            <p>School: {{$course->school->name}}<span class="label label-primary pull-right">${{$course->price}}</span></p>
+
                         </div>
                     </div>
                 </a>
             </div>
         @endif
     @endforeach
+    </div>
 
 @endsection
